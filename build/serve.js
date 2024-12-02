17224bedf092e7199f2f5a3e2496eb5720477037
Mark Kenny <beingmrkenny@gmail.com>
2024-12-02 08:01:00 +0000

    Buildy things better

--- build/serve.js
+++ build/serve.js
@@ -1,6 +1,7 @@
-const fs = require("fs-extra");
+const exec = require("child_process").execSync;
+const { compileJS } = require("./js");
 
-exports.prepareServeDirectory = () => {
+exports.serveDirectory = () => {
 	const fs = require("fs-extra");
 
 	fs.removeSync("serve");
@@ -21,4 +22,11 @@ exports.prepareServeDirectory = () => {
 	}
 };
 
-exports.prepareServeDirectory();
+exports.serveDirectory();
+
+compileJS();
+
+exec(
+	"cp app/index.html serve/index.html && sed -i '' -e \"s/?refresh=today/?refresh=$(echo $RANDOM | md5)/g\" serve/index.html;"
+);
+exec("sass scss/main.scss serve/words.css");
